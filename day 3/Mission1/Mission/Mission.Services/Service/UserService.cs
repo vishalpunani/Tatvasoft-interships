5a368ceec693929f7788f6d67fd1dc6f04557e90
vishalpunani <vishalpunani2004@gmail.com>
2025-07-14 13:25:55 +0530

    day 3

---
+++ day 3/Mission1/Mission/Mission.Services/Service/UserService.cs
@@ -0,0 +1,34 @@
+﻿using Mission.Entities.ViewModels;
+using Mission.Entities.ViewModels.Login;
+using Mission.Repositories.IRepository;
+using Mission.Services.IService;
+
+namespace Mission.Services.Service
+{
+    public class UserService(IUserRepository userRepository) : IUserService
+    {
+        private readonly IUserRepository _userRepository = userRepository;
+        public async Task<ResponseResult> LogiUser(UserLoginRequestModel model)
+        {
+            var (response, message) = await _userRepository.LogiUser(model);
+
+            var result = new ResponseResult()
+            {
+                Data = response,
+                Message = message
+            };
+
+            if (response == null)
+            {
+                result.Result = ResponseStatus.Error;
+            }
+            else
+            {
+
+                result.Result = ResponseStatus.Success;
+            }
+
+            return result;
+        }
+    }
+}
